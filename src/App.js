ba56d4947a5ff8546c24ba3ba60ef13739175c99
Emart99 <ezequielsimon22@yahoo.com.ar>
2022-04-16 19:24:59 -0300

    cambios en la estructura del proyecto

---
+++ src/App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useEffect, Component } from 'react';
+import { PERMISSIONS, requestMultiple } from 'react-native-permissions';
+import MainScreen from './views/MainScreen'
+import Registrarse from "./views/Registrarse"
+import Login from './views/Login'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen'
+import { LogBox } from "react-native";
+
+
+LogBox.ignoreLogs(["exported from 'deprecated-react-native-prop-types'.",]) // codigo hermoso, remplazar en un futuro
+
+const Stack = createNativeStackNavigator();
+const Permisos = [PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+PERMISSIONS.ANDROID.ACCESS_MEDIA_LOCATION,
+PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+PERMISSIONS.ANDROID.CAMERA]
+
+export default function App() {
+  useEffect(() => {
+    const requestPermisos = async () => {
+      const userResponse = await requestMultiple(Permisos);
+      return userResponse
+    }
+    requestPermisos().then(SplashScreen.hide())
+  }, [])
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Registrarse" component={Registrarse} options={{ headerShown: false }} />
+        <Stack.Screen name="MainScreen" component={MainScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -4,31 +4,22 @@ import styles from '../styles/styles'
 
 
 MapboxGL.setAccessToken('pk.eyJ1IjoiZXplYWtlbCIsImEiOiJjbDFyZGkzeGkwdmFhM2psbTRiZjNzZ252In0.lzoXhYbHmRVbuWyT3hNqiw');
-const Mapa = () =>{
-    
-    return(
-        <MapboxGL.MapView   style={styles.map}
-                            compassEnabled={false}
-                            styleURL={MapboxGL.StyleURL.Dark}>
-            
-            <MapboxGL.UserLocation androidRenderMode="gps" visible={true} />
-            <MapboxGL.Camera    followUserLocation={true}
-                                maxZoomLevel={25}
-                                minZoomLevel={7} />
+const Mapa = () => {
 
-            
-            
-                <MapboxGL.PointAnnotation 
-                                        id="test"
-                                        onSelected={()=>console.log("hola, fui presionado")}
-                                        coordinate={[-58.57745726877158,-34.54595351282419]}/>
-                                        
-            
-               
-          
-                       
-          
-            
+    return (
+        <MapboxGL.MapView style={styles.map}
+            compassEnabled={false}
+            styleURL={MapboxGL.StyleURL.Dark}>
+
+            <MapboxGL.UserLocation androidRenderMode="gps" visible={true} />
+            <MapboxGL.Camera followUserLocation={true}
+                maxZoomLevel={25}
+                minZoomLevel={7} />
+
+            <MapboxGL.PointAnnotation
+                id="test"
+                onSelected={() => console.log("hola, fui presionado")}
+                coordinate={[-58.57745726877158, -34.54595351282419]} />
         </MapboxGL.MapView>
     )
 }
