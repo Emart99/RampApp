2f793cff5bb585ac7463ea8e599fc2f42fe7c6be
Emart99 <ezequielsimon22@yahoo.com.ar>
2022-06-19 11:27:25 -0300

    login fixes = history.push / form usage / prevent default / labels ,

--- src/componentes/InputLogin.jsx
+++ src/componentes/InputLogin.jsx
@@ -6,10 +6,10 @@ import PropTypes from 'prop-types'
 
 export const InputLogin = ({ textoTitulo, type,value,onChange, onClick }) => {
     return (
-
         <div className="form-outline form-white mb-4">
+             {console.log(value)}
             <label className="form-label">{textoTitulo}</label>
-            <input  className="form-control text-center form-control-lg" type={type} placeholder={type} value={value} onClick = {onClick} onChange={(event) => onChange(event.target.value)} />
+            <input  className="form-control text-center form-control-lg" type={type} placeholder={textoTitulo} value={value} onClick = {onClick} onChange={(event) => onChange(event.target.value)} />
         </div>
     )
 }
--- src/vistas/Login.js
+++ src/vistas/Login.js
@@ -14,19 +14,19 @@ export function Login() {
     var history = useHistory()
 
 
-
-    const asignarUsuario = async() =>{
+    const asignarUsuario = async(event) =>{
+        event.preventDefault();
         try{  
           const login = new Administrador(0,"","","",userName,password)
           await adminService.getBuscarUsuario(login)
-          pushToDenunciasAVerificar()}
+          history.push('/denunciasAVerificar')}
           catch (error) {
               const message = obtenerMensaje(error)
               SetErrorMessage(message)
           }
       }
-
-      const pushToDenunciasAVerificar = () => history.push('/denunciasAVerificar')
+      
+      
     
       const limpiarError=()=>{
         SetErrorMessage("")
@@ -39,12 +39,11 @@ export function Login() {
               <div className="col-12 col-md-8 col-lg-6 col-xl-5">
                   <div className="card  login-card" style={{ borderRadiu: '1rem' }}>
                       <div className="card-body p-5 text-center">
-                      <form className="mb-md-4 mt-md-4 ">
+                      <form onSubmit={asignarUsuario} className="mb-md-4 mt-md-4 ">
                       <h1 className="fw-bold ">RampApp</h1>
-                      <InputLogin textoTitulo = "Usuario" type = "" value={userName} onClick= {()=>limpiarError()} onChange= {(nuevoUsuario) => setUserName(nuevoUsuario)} />
+                      <InputLogin textoTitulo = "Usuario" type ="username" value={userName} onClick= {()=>limpiarError()} onChange= {(nuevoUsuario) => setUserName(nuevoUsuario)} />
                       <InputLogin textoTitulo = "Contreaseña" type = "password" value={password} onClick= {()=>limpiarError()} onChange= {(nuevaContraseña) => setPassword(nuevaContraseña)} />    
-                      <button className="btn btn-primary btn-lg mt-4 px-5"
-                        onClick={() => asignarUsuario()} >
+                      <button type="submit" className="btn btn-primary btn-lg mt-4 px-5">
                         Ingresar
                       </button>
                       </form>
